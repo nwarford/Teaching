b1b8eb11be16b5d4daf73d60b2d7b54af24b520c
nwarford <noel.warford95@gmail.com>
2024-10-17 14:35:35 -0400

    code org example from lab 5

---
+++ code-organization.py
@@ -0,0 +1,16 @@
+
+def compare() :
+    print("I compare two strings of the same length!")
+    
+def best_alignment() :
+    compare()
+    print("I find the best alignment!")
+    
+def match_report() :
+    best_alignment()
+    print("I report matches!")
+
+def main() :
+    match_report()
+    
+main()
